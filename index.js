8832a1f766d3eccab13902362ce39fef92a8b94f
sa <sixsecondfiesta@gmail.com>
2016-05-02 20:51:28 -0400

    use local fonts, fix some bad stuff

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /*eslint semi: ["error", "never"]*/
-'use strict'
 
 const pluralize = require('pluralize')
 
-const r = (arr) => arr[Math.floor(Math.random() * (arr.length + 1))]
+const r = (arr) => arr[Math.floor(Math.random() * arr.length)]
 
 const upper = (str) => str[0].toUpperCase() + str.slice(1)
 const upperAll = (str) => str.split(' ').map(upper).join(' ').split('-').map(upper).join('-')
@@ -12,7 +11,8 @@ const weighted = (weights) => {
   const totalWeight = weights.reduce((p, c) => p + c[1], 0)
 
   let lastWeight = 0
-  for(const w of weights) {
+  for(let i=0; i<weights.length; i++) {
+    const w = weights[i]
     w[1] = w[1] / totalWeight + lastWeight
     lastWeight = w[1]
   }
@@ -22,7 +22,8 @@ const weighted = (weights) => {
 
   return () => {
     const rr = Math.random()
-    for(const w of weights) {
+    for(let i=0; i<weights.length; i++) {
+      const w = weights[i]
       if(rr < w[1]) {
         return w[0]
       }
@@ -32,35 +33,37 @@ const weighted = (weights) => {
 }
 
 
-// const adv = require('./corpora/data/words/adverbs').adverbs
-const adjectives = require('./corpora/data/words/adjs').adjs
-const metals = require('./corpora/data/materials/metals').metals
+const corpora = {
+  // adv: require('./corpora/data/words/adverbs').adverbs,
+  adjectives: require('./corpora/data/words/adjs').adjs,
+  metals: require('./corpora/data/materials/metals').metals,
 
-const animals = require('./corpora/data/animals/common').animals
-const appliances = require('./corpora/data/technology/appliances').appliances
-const colors = require('./corpora/data/colors/web_colors').colors.map(c => c.color.split(/(?=[A-Z])/).join(' '))
-const flowers = require('./corpora/data/plants/flowers').flowers
-const monsters = require('./corpora/data/mythology/monsters').names
-const rooms = require('./corpora/data/architecture/rooms').rooms
-const vegetables = require('./corpora/data/foods/vegetables').vegetables
+  animals: require('./corpora/data/animals/common').animals,
+  appliances: require('./corpora/data/technology/appliances').appliances,
+  colors: require('./corpora/data/colors/web_colors').colors.map(c => c.color.split(/(?=[A-Z])/).join(' ')),
+  flowers: require('./corpora/data/plants/flowers').flowers,
+  monsters: require('./corpora/data/mythology/monsters').names,
+  rooms: require('./corpora/data/architecture/rooms').rooms,
+  vegetables: require('./corpora/data/foods/vegetables').vegetables
+}
 
 
 const descriptor = weighted(
   [
-    [adjectives, 1],
-    [colors, 0.8],
-    [metals, 0.2]
+    [corpora.adjectives, 1],
+    [corpora.colors, 0.8],
+    [corpora.metals, 0.2]
   ]
 )
 
 const name = weighted(
   [
-    [animals, 1.3],
-    [appliances, 0.1],
-    [flowers, 0.2],
-    [monsters, 1.5],
-    [rooms, 0.1],
-    [vegetables, 0.25]
+    [corpora.animals, 1.3],
+    [corpora.appliances, 0.1],
+    [corpora.flowers, 0.2],
+    [corpora.monsters, 1.5],
+    [corpora.rooms, 0.1],
+    [corpora.vegetables, 0.25]
   ]
 )
 
